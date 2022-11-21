91d0ef89d24469c326f3941d1f41b13a2518189a
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2022-11-21 13:16:45 -0600

    Update progress bar with epoch and loss

--- src/train/train.py
+++ src/train/train.py
@@ -26,7 +26,7 @@ def train(image_size, num_channels, epochs, timesteps, sample_and_save_freq, sav
                 loss.backward()
                 optimizer.step()
 
-                pbar.set_postfix(Loss=f"{loss:.4f}")
+                pbar.set_postfix(Epoch=f"{epoch+1}/{epochs}", Loss=f"{loss:.4f}")
                 pbar.update()
 
                 visualize_loss(writer=writer, loss=loss, step=(epoch*len(dataloader) + step), loss_type=loss_type)
@@ -37,5 +37,6 @@ def train(image_size, num_channels, epochs, timesteps, sample_and_save_freq, sav
                     all_images = samples[-1] 
                     all_images = (all_images + 1) * 0.5
                     save_image_grid(step=(step*len(dataloader) + step), writer=writer, n_images=5, samples=all_images)
-        save_model(step=epoch, model=denoising_model)
+
+        save_model(epoch=epoch, model=denoising_model)
 
